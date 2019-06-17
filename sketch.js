7a532857d94c6ba99256ba43e16556c22f2d8ba4
Wayne Collier <ke4tri@gmail.com>
2019-06-17 12:36:19 -0500

    model is loading and consoled

--- sketch.js
+++ sketch.js
@@ -1,4 +1,14 @@
+let mobilenet;
+
+function modelReady() {
+  console.log("Model is ready!!!");
+}
+
 function setup() {
   createCanvas(640, 480);
   background(0);
+
+  mobilenet = ml5.imageClassifier('MobileNet', modelReady);
+
+
 }
