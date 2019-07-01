db1edf061bce758a74ea598bb8958243b9a961df
Wayne Collier <ke4tri@gmail.com>
2019-07-01 09:27:25 -0500

    image render in canvas and accessing model in cloud

--- image_classification/sketch.js
+++ image_classification/sketch.js
@@ -4,8 +4,14 @@ function modelReady() {
   console.log("Model is ready!!!");
 }
 
+function imageReady() {
+  image(puffin, 0, 0, width, height);
+}
+
 function setup() {
   createCanvas(640, 480);
+  puffin = createImg('images/puffin.jpg', imageReady);
+  puffin.hide();
   background(0);
 
   mobilenet = ml5.imageClassifier('MobileNet', modelReady);
