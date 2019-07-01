dc34a90065228406afd8853f8038f0e8561d43c5
Wayne Collier <ke4tri@gmail.com>
2019-07-01 11:03:57 -0500

    video from cam is displayed on canvas

--- image_classification/sketch.js
+++ image_classification/sketch.js
@@ -1,9 +1,10 @@
 let mobilenet;
-// let puffin;
+//let puffin;
+let video;
 
 function modelReady() {
   console.log("Model is ready!!!");
-  mobilenet.predict(puffin, gotResults);
+ // mobilenet.predict(puffin, gotResults);
 }
 
 function gotResults(error, results) {
@@ -11,20 +12,33 @@ function gotResults(error, results) {
     console.error(error);
   }else {
     console.log(results);
+    let label = results[0].className;
+    let prob = results[0].probability;
+    fill(0);
+    textSize(64);
+    text(label, 10, height - 100);
+    createP(label);
+    createP(prob);
   }
 }
 
-function imageReady() {
-  image(puffin, 0, 0, width, height);
-}
+//This would draw the image inside of the canvas if we were using a static image andnot
+//VIDEO
+// function imageReady() {
+//   image(puffin, 0, 0, width, height);
+// }
 
 function setup() {
   createCanvas(640, 480);
-  puffin = createImg('images/puffin.jpg', imageReady);
-  puffin.hide();
+  //Instead of using createImg (for just image), switch to createCapture
+  //puffin = createImg('images/puffin.jpg', imageReady);
+  video = createCapture(VIDEO);
+  video.hide();
+ // puffin.hide();
   background(0);
-
   mobilenet = ml5.imageClassifier('MobileNet', modelReady);
+}
 
-
+function draw() {
+  image(video, 0, 0);
 }
